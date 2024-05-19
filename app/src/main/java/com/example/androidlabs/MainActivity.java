91ef7b39446b6271b591c649414d7ae9a3f16645
KZeit <kyla.zeit@proton.me>
2024-05-19 01:19:10 -0400

    Lab 2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,24 +1,59 @@
 package com.example.androidlabs;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView result;
+    Button buttonSwitch;
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main_contraint);
+
+        editText = (EditText) findViewById(R.id.edit_text);
+        result = (TextView) findViewById(R.id.text_view);
+        buttonSwitch = (Button) findViewById(R.id.press_btn);
+        checkBox = (CheckBox) findViewById(R.id.check_box);
+
+        final AppCompatActivity context = this;
+        buttonSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                result.setText(text);
+                Toast.makeText(context, getString(R.string.toast_message), Toast.LENGTH_LONG).show();
+            }
         });
+
+        checkBox.setOnCheckedChangeListener(new
+                                                    CompoundButton.OnCheckedChangeListener() {
+                                                        @Override
+                                                        public void onCheckedChanged(CompoundButton cb, boolean isChecked) {
+                                                            if (isChecked) {
+                                                                Snackbar.make(cb, getString(R.string.snack_on), Snackbar.LENGTH_LONG)
+                                                                        .setAction("undo", click -> cb.setChecked(!isChecked))
+                                                                        .show();
+                                                            } else {
+                                                                Snackbar.make(cb, getString(R.string.snack_off), Snackbar.LENGTH_LONG)
+                                                                        .setAction("undo", click -> cb.setChecked(!isChecked))
+                                                                        .show();
+                                                            }
+                                                        }
+                                                    });
     }
 }
+
+
