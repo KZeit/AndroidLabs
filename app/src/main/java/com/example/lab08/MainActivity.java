5bdf6b9a6d6515710cd93347fdceb00046488962
KZeit <kyla.zeit@proton.me>
2024-06-29 16:26:57 -0400

    Lab 8

---
+++ app/src/main/java/com/example/lab08/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.lab08;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_item1) {
+            Toast.makeText(this, "You clicked on item 1", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.action_item2) {
+            Toast.makeText(this, "You clicked on item 2", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+         if (id == R.id.nav_dad_joke) {
+            // Navigate to DadJokeActivity
+            Intent intent = new Intent(this, DadJokeActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_exit) {
+            finishAffinity();
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+}
